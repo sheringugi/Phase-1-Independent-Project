01e9ec850ede424ba6c1df310d6097f3b176dead
sheringugi <sheri.ngugi17@gmail.com>
2023-04-04 21:30:14 +0300

    Iterating through data on API

--- index.js
+++ index.js
@@ -12,13 +12,25 @@
         // Display the list of cocktails searched in cards
         const list= document.querySelector('#cocktailsList');
         
-        let cocktailCard = `
-        <div class="card" style="width:400px">
+        let cocktailCard = `<div class="card" style="width:400px">
             <div class="card-body">
-                <h4 class="card-title">_COCKTAILNAME_</h4>
-                <img class="card-img-bottom" src="_COCKTAILIMAGE_" alt="_COCKTAILNAME_" style="width:100%">
-                <p class="card-text">_INGREDIENTS_</p>        
-            </div>
+            <h4 class="card-title">_COCKTAILNAME_</h4>
+            <img class="card-img-bottom" src="_COCKTAILIMAGE_" alt="_COCKTAILNAME_" style="width:100%">
+            <p class="card-text">_INGREDIENTS_</p>        
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal_MODALNUMBER_">How to Brew</button>
+                        </div>
         </div>
       `
+      //create empty string to hold each cocktail upon iteration
+        let aggregateCocktails= ""
+        let count= 1
+        let cocktailRow=`<div class="row">`
+        //create empty string to hold individual cocktail card 
+        let tempCocktailCard=""
+        cocktails.drinks.forEach(drink => {
+            if (cocktail == null) {
+                search = drink.strDrink
+            }
+        });
     });
+            
