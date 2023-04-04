68d11d56c71db02478bdee46146735882f0e42d7
sheringugi <sheri.ngugi17@gmail.com>
2023-04-04 22:02:50 +0300

    Add data into modal box

--- index.js
+++ index.js
@@ -60,11 +60,31 @@
             if (cocktail == null) {
                 search = drink.strDrink
             }
+
+            //create empty string to hold ingredients of individual cocktail to be held by modal box
+            let ingredients = ""
+            ingredients  += (drink.strIngredient1 === null) ? '' : drink.strIngredient1 + ', ';
+            ingredients  += (drink.strIngredient2 === null) ? '' : drink.strIngredient2 + ', ';
+            ingredients  += (drink.strIngredient3 === null) ? '' : drink.strIngredient3 + ', ';
+            ingredients  += (drink.strIngredient4 === null) ? '' : drink.strIngredient4 + ', ';
+            ingredients  += (drink.strIngredient5 === null) ? '' : drink.strIngredient5 + ', ';
+            ingredients  += (drink.strIngredient6 === null) ? '' : drink.strIngredient6 + ', ';
+            ingredients  += (drink.strIngredient7 === null) ? '' : drink.strIngredient7 + ', ';
+            ingredients  += (drink.strIngredient8 === null) ? '' : drink.strIngredient8 + ', ';
+            ingredients  += (drink.strIngredient9 === null) ? '' : drink.strIngredient9 + ', ';
+            ingredients  += (drink.strIngredient10 === null) ? '' : drink.strIngredient10 + ', ';
+            ingredients  += (drink.strIngredient11 === null) ? '' : drink.strIngredient11 + ', ';
+            ingredients  += (drink.strIngredient12 === null) ? '' : drink.strIngredient12 + ', ';
+            ingredients  += (drink.strIngredient13 === null) ? '' : drink.strIngredient13 + ', ';
+            ingredients  += (drink.strIngredient14 === null) ? '' : drink.strIngredient14 + ', ';
+            ingredients  += (drink.strIngredient15 === null) ? '' : drink.strIngredient15 + '.';
+
             
             //replace placeholders with the data to be dispalyed in the cards
             tempCocktailCard= cocktailCard
             tempCocktailCard = tempCocktailCard.replace(/_COCKTAILIMAGE_/g, drink.strDrinkThumb);
             tempCocktailCard = tempCocktailCard.replace(/_COCKTAILNAME_/g, drink.strDrink);
+            tempCocktailCard = tempCocktailCard.replace(/_INGREDIENTS_/g, ingredients);
             
             
             // limit the number of cards displayed on one row to four
